d8dbd8311c29357627ef2155a6724ed5adb1bf19
git <git@cn.itcast>
2019-08-23 14:32:15 +0800

    分支添加

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -1,9 +1,17 @@
 import org.junit.Test;
 
 public class Main {
+	
 
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
+		
     }
+	
+	@Test
+	public viod test2(){
+		System.out.println("test11");
+	}
+	
 }
